e6319d29e706b4259e63e45171e4928e9730a58c
ajlopez <webmaster@ajlopez.com>
2013-08-03 19:33:49 -0300

    Additional digits test

--- test/digits.js
+++ test/digits.js
@@ -12,4 +12,13 @@ assert.ok(Array.isArray(tokens));
 assert.equal(tokens.length, 1);
 assert.equal(tokens[0].value, '1');
 assert.equal(tokens[0].type, Types.Digits);
+    
+// get digits
+
+var tokens = kodetokenizer.getTokens('123');
+assert.ok(tokens);
+assert.ok(Array.isArray(tokens));
+assert.equal(tokens.length, 1);
+assert.equal(tokens[0].value, '123');
+assert.equal(tokens[0].type, Types.Digits);
 
