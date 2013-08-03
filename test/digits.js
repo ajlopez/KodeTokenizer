b2777e1b96c0c0aff2a863e6b283915d778690df
ajlopez <webmaster@ajlopez.com>
2013-08-03 19:47:04 -0300

    Recognize word + digits and digits + word

--- test/digits.js
+++ test/digits.js
@@ -22,3 +22,13 @@ assert.equal(tokens.length, 1);
 assert.equal(tokens[0].value, '123');
 assert.equal(tokens[0].type, Types.Digits);
 
+// get digits and word
+
+var tokens = kodetokenizer.getTokens('123abc');
+assert.ok(tokens);
+assert.ok(Array.isArray(tokens));
+assert.equal(tokens.length, 2);
+assert.equal(tokens[0].value, '123');
+assert.equal(tokens[0].type, Types.Digits);
+assert.equal(tokens[1].value, 'abc');
+assert.equal(tokens[1].type, Types.Word);
--- test/word.js
+++ test/word.js
@@ -21,3 +21,14 @@ assert.ok(Array.isArray(tokens));
 assert.equal(tokens.length, 1);
 assert.equal(tokens[0].value, 'A');
 assert.equal(tokens[0].type, Types.Word);
+
+// get word and digits
+
+var tokens = kodetokenizer.getTokens('abc123');
+assert.ok(tokens);
+assert.ok(Array.isArray(tokens));
+assert.equal(tokens.length, 2);
+assert.equal(tokens[0].value, 'abc');
+assert.equal(tokens[0].type, Types.Word);
+assert.equal(tokens[1].value, '123');
+assert.equal(tokens[1].type, Types.Digits);
