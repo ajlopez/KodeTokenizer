41925942aa97968c13bf712c2312038cb5182e11
ajlopez <webmaster@ajlopez.com>
2013-08-03 19:50:24 -0300

    Additional test: tab as whitespace

--- test/whitespace.js
+++ test/whitespace.js
@@ -12,3 +12,12 @@ assert.ok(Array.isArray(tokens));
 assert.equal(tokens.length, 1);
 assert.equal(tokens[0].value, ' ');
 assert.equal(tokens[0].type, Types.WhiteSpace);
+
+// tab as whitespace
+
+var tokens = kodetokenizer.getTokens('\t');
+assert.ok(tokens);
+assert.ok(Array.isArray(tokens));
+assert.equal(tokens.length, 1);
+assert.equal(tokens[0].value, '\t');
+assert.equal(tokens[0].type, Types.WhiteSpace);
