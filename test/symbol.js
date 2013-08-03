9f3335a9e188b599594326a2793b9364d6afce3d
ajlopez <webmaster@ajlopez.com>
2013-08-03 20:18:38 -0300

    Multi-character symbols

--- test/symbol.js
+++ test/symbol.js
@@ -12,3 +12,21 @@ assert.ok(Array.isArray(tokens));
 assert.equal(tokens.length, 1);
 assert.equal(tokens[0].value, '=');
 assert.equal(tokens[0].type, Types.Symbol);
+
+// get == as symbol
+
+var tokens = kodetokenizer.getTokens('==');
+assert.ok(tokens);
+assert.ok(Array.isArray(tokens));
+assert.equal(tokens.length, 1);
+assert.equal(tokens[0].value, '==');
+assert.equal(tokens[0].type, Types.Symbol);
+
+// get !== as symbol
+
+var tokens = kodetokenizer.getTokens('!==');
+assert.ok(tokens);
+assert.ok(Array.isArray(tokens));
+assert.equal(tokens.length, 1);
+assert.equal(tokens[0].value, '!==');
+assert.equal(tokens[0].type, Types.Symbol);
